bd676eec8226d0e66a40db9016d9c6b843e0374f
JamboNo5 <jatommo@gmail.com>
2023-03-13 17:00:52 +0000

    Setup generator member variables

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
+    // The number of floors generated for the dungeon
+    [SerializeField] const int  m_FloorLimit = 1;
+    // The number of rooms for the floor
+    [SerializeField] const int  m_RoomLimit = 1;
+    // The number of rooms currently in the queue
+    [SerializeField] const int  m_RoomCount = 0;
+    // The 2D array which contains all the data about the current floor.
+    Room[][]                    m_MapArray;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
