67c1fdfc2e8b3cdc9a4b7ae9de5012fe2f365c48
JamboNo5 <jatommo@gmail.com>
2023-03-13 16:10:02 +0000

    Add room script

---
+++ Assets/Scenes/Room.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Room
+{
+    public Vector2 m_Pos;
+    public bool m_RoomAdded = false;
+    [SerializeField] GameObject[] m_EnemyArray;
+    [SerializeField] Vector2[] m_SpawnPoints;
+    [SerializeField] Tilemap m_Tilemap;
+}
