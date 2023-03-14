d3c2b9d3727bead63abc4568abd66ca37586c624
JamboNo5 <jatommo@gmail.com>
2023-03-14 16:49:48 +0000

    Change alignment and remove unnecessary system.serializable line

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-[System.Serializable]
 public class DungeonGenerator : MonoBehaviour
 {
     // The number of floors generated for the dungeon
-    [SerializeField] int  m_FloorLimit;
+    [SerializeField] int    m_FloorLimit;
     // The number of rooms for the floor
-    [SerializeField] int  m_RoomLimit;
+    [SerializeField] int    m_RoomLimit;
     // The number of rooms currently in the queue
-    [SerializeField] int  m_RoomCount;
+    [SerializeField] int    m_RoomCount;
 
     // Array containing all the floors of a dungeon
     SceneAsset[] m_Floors;
 
     // Array containing all possible rooms to be used in the dungeon
-    [SerializeField] Room[]     m_RoomVariants;
+    [SerializeField] Room[] m_RoomVariants;
 
     // Start is called before the first frame update
     void Start()
