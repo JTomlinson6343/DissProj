20af18435200a818eb1ef92c9dc351833064800f
JamboNo5 <jatommo@gmail.com>
2023-03-14 16:00:23 +0000

    Change values to allow inspector to see them

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
+[System.Serializable]
 public class DungeonGenerator : MonoBehaviour
 {
     // The number of floors generated for the dungeon
-    [SerializeField] const int  m_FloorLimit = 1;
+    [SerializeField] int  m_FloorLimit;
     // The number of rooms for the floor
-    [SerializeField] const int  m_RoomLimit = 1;
+    [SerializeField] int  m_RoomLimit;
     // The number of rooms currently in the queue
-    [SerializeField] const int  m_RoomCount = 0;
-    // Array containing all the floors of a dungeon.
-    Floor[]                     m_Floors;
+    [SerializeField] int  m_RoomCount;
+
+    // Array containing all the floors of a dungeon
+    SceneAsset[] m_Floors;
+
+    // Array containing all possible rooms to be used in the dungeon
+    [SerializeField] Room[]     m_RoomVariants;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        return;
     }
 }
