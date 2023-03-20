32ada91a27c995f863dbf3c569a70799a8f8ac23
James Tomlinson <jatommo@gmail.com>
2023-03-20 15:51:16 +0000

    Move floor generation logic into floor class

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -10,88 +10,29 @@ public class DungeonGenerator : MonoBehaviour
 {
     // The number of floors generated for the dungeon
     [SerializeField] int    m_FloorLimit;
-    // The number of rooms for the floor
-    [SerializeField] int    m_RoomLimit;
-
-    // Array containing all possible rooms to be used in the dungeon
-    [SerializeField] Room[] m_RoomVariants;
-
-    // The number of rooms currently in the queue
-    int m_RoomCount;
-
-    // Array containing all the floors of a dungeon
-    Floor[] m_Floors;
 
     // The size (width,height) of the floor
     [SerializeField] int m_FloorWidth;
     [SerializeField] int m_FloorHeight;
 
-    // Positionn of the starting room
-    Vector2Int m_StartRoomPos;
+    // Array containing all possible rooms to be used in the dungeon
+    [SerializeField] Room[] m_RoomVariants;
+
+    // The number of rooms for the floor
+    [SerializeField] int m_RoomLimit;
+
+    // Array containing all the floors of a dungeon
+    Floor[] m_Floors;
 
     // Start is called before the first frame update
     void Start()
     {
         // Starting room is placed at the centre of the floor
-        m_StartRoomPos = new Vector2Int(m_FloorWidth / 2, m_FloorHeight / 2);
 
-        GenerateFloor();
-        return;
-    }
-
-    void GenerateFloor()
-    {
-
-        // Make queue of rooms
-        Queue<Room> roomQueue = new Queue<Room>();
-
-        // Instantiate starting room
-        StartRoom startRoom = new StartRoom();
-
-        // Instantiate floor
-        Floor floor = new Floor();
-
-        // Init array to store rooms
-        Room[,] mapArray = new Room[m_FloorWidth, m_FloorHeight];
-
-        // Add starting room to array
-        InitRoom(m_StartRoomPos, mapArray, startRoom);
-
-        Vector2Int[] directionArray = {
-            new Vector2Int(0, -1), new Vector2Int(1, 0),
-            new Vector2Int(0, 1), new Vector2Int(-1, 0) 
-        };
-
-        while (m_RoomCount < m_RoomLimit)
+        for (int i = 0; i < m_FloorLimit;)
         {
-            
-            foreach (Room room in roomQueue)
-            {
-                foreach(Vector2Int dir in directionArray)
-                {
-                    // If a 50/50 chance happens, skip to the next possible room slot
-                    if (Random.Range(0, 1) == 1)
-                        continue;
-
-                    Vector2Int pos = room.m_Pos + dir;
-
-                    // Check if new cell is not already occupied
-                    if (mapArray[pos.x, pos.y] != null)
-                        continue;
-
-                    // Check there arent already too many neighbours
-
-                }
-            }
+            m_Floors[i] = new Floor(m_FloorWidth, m_FloorHeight, m_RoomLimit);
         }
-    }
-
-
-    void InitRoom(Vector2Int pos, Room[,] mapArray, Room room)
-    {
-        // Place room in the position passed
-        mapArray[pos.x,pos.y] = room;
-        // Set position of room to position passed
-        room.m_Pos = pos;
+        return;
     }
 }
--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -7,4 +7,93 @@ public class Floor
 {
     // The 2D array which contains all the data about the current floor.
     public Room[,] m_MapArray;
+
+    // The number of rooms for the floor
+    int m_RoomLimit;
+
+    // The number of rooms currently in the queue
+    int m_RoomCount;
+
+    // The size (width,height) of the floor
+    int m_FloorWidth;
+    int m_FloorHeight;
+
+    // Queue of rooms to loop over
+    Queue<Room> roomQueue = new Queue<Room>();
+
+    // Direction lookup table to loop over when checking neighbours
+    Vector2Int[] directionArray = {
+            new Vector2Int(0, -1), new Vector2Int(1, 0),
+            new Vector2Int(0, 1), new Vector2Int(-1, 0)
+        };
+
+    // Positionn of the starting room
+    Vector2Int m_StartRoomPos;
+
+    public Floor(int width, int height, int roomLimit)
+    {
+        m_RoomLimit = roomLimit;
+        m_FloorWidth = width;
+        m_FloorHeight = height;
+
+        GenerateFloor();
+    }
+
+    void GenerateFloor()
+    {
+        m_StartRoomPos = new Vector2Int(m_FloorWidth / 2, m_FloorHeight / 2);
+
+        // Instantiate starting room
+        StartRoom startRoom = new StartRoom();
+
+        // Init array to store rooms
+        m_MapArray = new Room[m_FloorWidth, m_FloorHeight];
+
+        // Add starting room to array
+        InitRoom(m_StartRoomPos, startRoom);
+
+        while (m_RoomCount < m_RoomLimit)
+        {
+
+            foreach (Room room in roomQueue)
+            {
+                foreach (Vector2Int dir in directionArray)
+                {
+                    // If a 50/50 chance happens, skip to the next possible room slot
+                    if (Random.Range(0, 1) == 1)
+                        continue;
+
+                    Vector2Int pos = room.m_Pos + dir;
+
+                    // Check if new cell is not already occupied
+                    if (m_MapArray[pos.x, pos.y] != null)
+                        continue;
+
+                    // Check there arent already too many neighbours
+
+                }
+            }
+        }
+    }
+
+
+    void InitRoom(Vector2Int pos, Room room)
+    {
+        // Place room in the position passed
+        m_MapArray[pos.x, pos.y] = room;
+        // Set position of room to position passed
+        room.m_Pos = pos;
+    }
+
+    bool TooManyNeighboursCheck(Vector2Int pos, Room[,] mapArray)
+    {
+        int neighbourCount = 0;
+
+        foreach (Vector2Int dir in direc)
+        {
+
+        }
+
+        return true;
+    }
 }
